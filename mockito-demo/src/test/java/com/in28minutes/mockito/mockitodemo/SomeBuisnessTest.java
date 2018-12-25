9219be1388b9f08ed3ab4e893ed9fdc295e7a774
wojtekDrozd <wojdro89@gmail.com>
2018-12-25 11:45:38 +0100

    Initial commit

---
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBuisnessTest.java
@@ -0,0 +1,25 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeBuisnessTest {
+
+	@Test
+	public void TestFindGreatestFromAllData() {
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub());
+		int result = businessImpl.findGreatestFromAllData();
+		assertEquals(24,result);
+	}
+
+}
+
+class DataServiceStub implements DataService{
+
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] {24,6,15};
+	}
+	
+}
